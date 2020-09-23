43a04940c142abb5a9e1e4e7d4fcbe919feddd50
SOHAIL1996 <salman.omar.sohail@gmail.com>
2020-09-23 22:28:55 +0200

    Completed Navgation

--- src/RSG.py
+++ src/RSG.py
@@ -30,6 +30,7 @@ from geometry_msgs.msg import *
 from gazebo_msgs.srv import SpawnModel
 
 from utilities.utility import bound_box
+from utilities.utility import Configuration
 from utilities.utility import choices
 from termcolor import colored
 
@@ -185,64 +186,7 @@ class Model():
             time.sleep(2)
             delete_node.terminate()   
             print(colored('Successfully deleted {0} model'.format(model_name),'green'))   
-
-class Configuration():
-
-    def __init__(self):
-        
-        self.rospakg       = 'atg'
-        self.ros_file_name = 'hsr.launch'
-
-        with open("src/utilities/config.txt",mode='r') as file:
-            data    = file.readlines()
-            # Index starts from 0 select -1 line
-            launch_dir = data[4]
-            model_dir  = data[5]
-            num_of_mod = data[8]
-            models_1   = data[9]
-            models_2   = data[10]
-
-            launch_dir             = launch_dir.split('=')
-            model_dir              = model_dir.split('=')
-            num_of_mod             = num_of_mod.split('=')
-            models_1               = models_1.split('=')
-            models_2               = models_2.split('=')
-
-            self.launch_dir         = launch_dir[1].strip()
-            self.model_dir          = model_dir[1].strip()
-            self.num_of_mod         = num_of_mod[1].strip()
-            self.models             = models_1[1].strip()+models_2[1].strip()
-
-    def model_uri_correcter(self, model_name):
-        """
-        This function opens the sdf model files and corrects the uri tags. This is useful when changing the
-        paths of the model folder. 
-
-        Args:
-            model_name (str): The model name for the uri tag correction.
-        """        
-        with open(os.path.realpath('.')+'/Environment/models/'+model_name+'/'+model_name+'.sdf',mode='r') as file:
-                counter    = 0
-                data       = file.readlines()
-                save_index = []
-
-                for word in data:
-                    if (word.find('uri') != -1): 
-                        save_index.append(counter) 
-                    counter +=1
-                
-                data[save_index[0]] = '             <uri>'+self.model_dir+'/'+model_name+'/meshes/'+model_name+'.stl</uri>\n'
-                data[save_index[1]] = '             <uri>'+self.model_dir+'/'+model_name+'/meshes/'+model_name+'.stl</uri>\n'
-                data[save_index[2]] = '             <uri>'+self.model_dir+'/'+model_name+'/materials/scripts</uri>\n'
-                data[save_index[3]] = '             <uri>'+self.model_dir+'/'+model_name+'/materials/textures</uri>\n'
-        
-        with open(os.path.realpath('.')+'/Environment/models/'+model_name+'/'+model_name+'.sdf',mode='w') as file:
-            file.writelines(data)
-            
-    def model_list(self):
-        all_models = self.models.split(',')
-        return all_models
-           
+          
 def collision_checker(model,x,y,z):
     """Checks the collision with all other dynamic models. Uses 
     Axis-Aligned Bounding Box.
@@ -258,30 +202,30 @@ def collision_checker(model,x,y,z):
               Returns False if there is a valid position for given model.
     """    
     # original bounding box
-    org_sx = np.around(model.bounding_box[0]+x,2)
-    org_bx = np.around(model.bounding_box[1]+x,2)
-    org_sy = np.around(model.bounding_box[2]+y,2)
-    org_by = np.around(model.bounding_box[3]+y,2)
-    org_sz = np.around(model.bounding_box[4]+z,2)
-    org_bz = np.around(model.bounding_box[5]+z,2)
+    org_sx = np.around(model.bounding_box[0]+x,2)+0.6
+    org_bx = np.around(model.bounding_box[1]+x,2)+0.6
+    org_sy = np.around(model.bounding_box[2]+y,2)+0.6
+    org_by = np.around(model.bounding_box[3]+y,2)+0.6
+    org_sz = np.around(model.bounding_box[4]+z,2)+0.6
+    org_bz = np.around(model.bounding_box[5]+z,2)+0.6
         
     if model_tracer != []: 
              
         for models in np.arange(len(model_tracer)): 
             # compared bounding box
-            com_sx = np.around(model_tracer[models].bounding_box[0]+float(model_tracer[models].x_coord),2)
-            com_bx = np.around(model_tracer[models].bounding_box[1]+float(model_tracer[models].x_coord),2)
-            com_sy = np.around(model_tracer[models].bounding_box[2]+float(model_tracer[models].y_coord),2)
-            com_by = np.around(model_tracer[models].bounding_box[3]+float(model_tracer[models].y_coord),2)
-            com_sz = np.around(model_tracer[models].bounding_box[4]+float(model_tracer[models].z_coord),2)
-            com_bz = np.around(model_tracer[models].bounding_box[5]+float(model_tracer[models].z_coord),2)
+            com_sx = np.around(model_tracer[models].bounding_box[0]+float(model_tracer[models].x_coord),2)+0.6
+            com_bx = np.around(model_tracer[models].bounding_box[1]+float(model_tracer[models].x_coord),2)+0.6
+            com_sy = np.around(model_tracer[models].bounding_box[2]+float(model_tracer[models].y_coord),2)+0.6
+            com_by = np.around(model_tracer[models].bounding_box[3]+float(model_tracer[models].y_coord),2)+0.6
+            com_sz = np.around(model_tracer[models].bounding_box[4]+float(model_tracer[models].z_coord),2)+0.6
+            com_bz = np.around(model_tracer[models].bounding_box[5]+float(model_tracer[models].z_coord),2)+0.6
             
             # Collision X-Y-Z-element
-            if (org_sx < com_bx and org_bx > com_sx and org_sy < com_by and org_by > com_sy and org_sz < com_bz and org_bz > com_sz):
-                return True
-            # Collision X-Y-element
-            # if (org_sx < com_bx and org_bx > com_sx and org_sy < com_by and org_by > com_sy):
+            # if (org_sx < com_bx and org_bx > com_sx and org_sy < com_by and org_by > com_sy and org_sz < com_bz and org_bz > com_sz):
             #     return True
+            # Collision X-Y-element
+            if (org_sx < com_bx and org_bx > com_sx and org_sy < com_by and org_by > com_sy):
+                return True
 
         return False
     else:
@@ -305,7 +249,7 @@ if __name__ == "__main__":
         static_model.x_coord, static_model.y_coord, static_model.z_coord = '0','4','0'
 
         # Dynamic model placement
-        model_list = choices(conf.model_list(), k=int(conf.num_of_mod))
+        model_choices = choices(conf.model_list(), k=int(conf.num_of_mod))
         
         # Ensuring the models spawns in the vicinity of the static model in this case the table
         minx = np.around(static_model.bounding_box[0]*0.8+float(static_model.x_coord),2)
@@ -315,7 +259,7 @@ if __name__ == "__main__":
         minz = np.around(static_model.bounding_box[4]+float(static_model.z_coord),2)
         maxz = np.around(static_model.bounding_box[5]+float(static_model.z_coord),2)
 
-        for model in model_list:
+        for model in model_choices:
             dynamic_model = Model(model)
                                     
             # Ensuring models don't spawn on top of existing models
--- src/utilities/utility.py
+++ src/utilities/utility.py
@@ -5,9 +5,67 @@ from stl import mesh
 from itertools import accumulate as _accumulate, repeat as _repeat
 from bisect import bisect as _bisect
 
+class Configuration():
+    
+    def __init__(self):
+        
+        self.rospakg       = 'atg'
+        self.ros_file_name = 'hsr.launch'
+
+        with open("src/utilities/config.txt",mode='r') as file:
+            data    = file.readlines()
+            # Index starts from 0 select -1 line
+            launch_dir = data[4]
+            model_dir  = data[5]
+            num_of_mod = data[8]
+            models_1   = data[9]
+            models_2   = data[10]
+
+            launch_dir             = launch_dir.split('=')
+            model_dir              = model_dir.split('=')
+            num_of_mod             = num_of_mod.split('=')
+            models_1               = models_1.split('=')
+            models_2               = models_2.split('=')
+
+            self.launch_dir         = launch_dir[1].strip()
+            self.model_dir          = model_dir[1].strip()
+            self.num_of_mod         = num_of_mod[1].strip()
+            self.models             = models_1[1].strip()+models_2[1].strip()
+
+    def model_uri_correcter(self, model_name):
+        """
+        This function opens the sdf model files and corrects the uri tags. This is useful when changing the
+        paths of the model folder. 
+
+        Args:
+            model_name (str): The model name for the uri tag correction.
+        """        
+        with open(os.path.realpath('.')+'/Environment/models/'+model_name+'/'+model_name+'.sdf',mode='r') as file:
+                counter    = 0
+                data       = file.readlines()
+                save_index = []
+
+                for word in data:
+                    if (word.find('uri') != -1): 
+                        save_index.append(counter) 
+                    counter +=1
+                
+                data[save_index[0]] = '             <uri>'+self.model_dir+'/'+model_name+'/meshes/'+model_name+'.stl</uri>\n'
+                data[save_index[1]] = '             <uri>'+self.model_dir+'/'+model_name+'/meshes/'+model_name+'.stl</uri>\n'
+                data[save_index[2]] = '             <uri>'+self.model_dir+'/'+model_name+'/materials/scripts</uri>\n'
+                data[save_index[3]] = '             <uri>'+self.model_dir+'/'+model_name+'/materials/textures</uri>\n'
+        
+        with open(os.path.realpath('.')+'/Environment/models/'+model_name+'/'+model_name+'.sdf',mode='w') as file:
+            file.writelines(data)
+            
+    def model_list(self):
+        all_models = self.models.split(',')
+        return all_models
+    
 def mesh_extractor(model):
     # model_directory = os.path.dirname(__file__)+'/Environment/models/'+ model +'/meshes/'+ model +'.stl'
-    model_directory = os.path.realpath('.')+'/Environment/models/'+ model +'/meshes/'+ model +'.stl'
+    conf = Configuration()
+    model_directory = conf.model_dir+'/'+ model +'/meshes/'+ model +'.stl'
     model_mesh = mesh.Mesh.from_file(model_directory)
     return model_mesh
     
